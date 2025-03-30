f0595877324099a27fdf30c2db18cc0cb52595a9
Dipam Sen <dipamsen.fp@gmail.com>
2025-03-30 16:35:50 +0000

    update styles

--- presentation/src/main.ts
+++ presentation/src/main.ts
@@ -1,6 +1,10 @@
 import Reveal from "reveal.js";
+import RevealHighlight from "reveal.js/plugin/highlight/highlight.esm";
 import "reveal.js/dist/reveal.css";
+import "reveal.js/plugin/highlight/monokai.css";
 import "./style.css";
 import "./theme.css";
 
-Reveal.initialize();
+Reveal.initialize({
+  plugins: [RevealHighlight],
+});
